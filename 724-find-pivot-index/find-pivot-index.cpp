1b629a9d2b2159cc3bf4d6f4be9f9b4cd8c7dde0
Nada Elokaily <nadaelokaily@gmail.com>
2024-05-05 18:28:38 +0300

    Time: 17 ms (55.40%) | Memory: 34.6 MB (23.55%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int size = nums.size();
-        vector<int> pre(size), post(size) ;
+        int size = nums.size(), post_c, post_p;
+        vector<int> pre(size) ;
         pre[0] = nums[0];
         for(int i = 1; i<size; i++){
             pre[i] = pre[i-1] + nums[i];
         }
-        post[0] = pre[size-1];
-        for(int i = 1; i<size; i++){
-            post[i] = post[i-1] - nums[i-1];
-        }
-        for(int i = 0; i< size; i++){
-            if(post[i] == pre[i]) return i;
+        for(int i = 0; i<size; i++){
+            if (i==0)
+                post_c = pre[size-1];
+            else
+                post_c = post_p - nums[i-1];
+            if(post_c == pre[i]) return i;
+            post_p = post_c;
         }
         return -1;
     }
